041089bea0ffb8bc990c53c88de9af63a0389ec7
Seulgi <jusanha17@gmail.com>
2026-01-23 16:16:00 +0900

    init: spring 세팅

---
+++ src/main/java/com/goormgb/be/demo/GoormgbApplication.java
@@ -0,0 +1,13 @@
+package com.goormgb.be.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormgbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoormgbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goormgb/be/demo/GoormgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormgb.be.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
