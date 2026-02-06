2b2ea53d673ea58fd6fec3813f98e02bbbdb3612
ejinn1 <ejinn1@naver.com>
2026-02-06 13:38:24 +0900

    feat(onboarding): 온보딩 선호 값 수정 api

--- src/main/java/com/goormgb/be/onboarding/controller/OnboardingPreferenceController.java
+++ src/main/java/com/goormgb/be/onboarding/controller/OnboardingPreferenceController.java
@@ -2,14 +2,12 @@ package com.goormgb.be.onboarding.controller;
 
 import com.goormgb.be.global.response.ApiResult;
 import com.goormgb.be.onboarding.dto.request.OnboardingPreferenceCreateRequest;
+import com.goormgb.be.onboarding.dto.request.OnboardingPreferenceUpdateRequest;
 import com.goormgb.be.onboarding.dto.response.OnboardingPreferenceCreateResponse;
 import com.goormgb.be.onboarding.dto.response.OnboardingPreferenceGetResponse;
 import com.goormgb.be.onboarding.service.OnboardingPreferenceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -41,5 +39,18 @@ public class OnboardingPreferenceController {
         var response = onboardingPreferenceService.createPreferences(userId, request);
 
         return ApiResult.ok(response);
+    }
+
+    @PutMapping("/preferences")
+    public ApiResult<Void> updatePreferences(
+            // TODO: token 기반으로 유저 가져오기
+            // @AuthenticationPrincipal CurrentUser currentUser
+            OnboardingPreferenceUpdateRequest request
+    ) {
+        // TODO: currentUser.userId()로 변경
+        Long userId = 1L;
+        onboardingPreferenceService.updatePreferences(userId, request);
+
+        return ApiResult.ok();
     }
 }
