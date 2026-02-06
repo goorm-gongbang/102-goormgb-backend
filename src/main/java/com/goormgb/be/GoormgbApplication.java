2672f770befe0d055eedd61a8b8c750fa45dbcd0
Seulgi <117719441+SeulGi0117@users.noreply.github.com>
2026-02-06 21:23:03 +0900

    refactor(login-domain): 카카오 OAuth 자잘한 수정들 (#17)
    - KakaoOAuthProperties: @Configuration 제거, @ConfigurationPropertiesScan으로 변경
    - KakaoUserResponse: private static 내부 클래스를 package-private으로 변경 (Jackson 역직렬화 호환)
    - KakaoOAuthClient: 미사용 import 제거, Javadoc 오타 수정, 불필요한 지역변수 제거
    - KakaoUserResponse: FQCN 대신 import된 Optional 사용으로 통일

--- src/main/java/com/goormgb/be/GoormgbApplication.java
+++ src/main/java/com/goormgb/be/GoormgbApplication.java
@@ -2,8 +2,10 @@ package com.goormgb.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class GoormgbApplication {
 
 	public static void main(String[] args) {
