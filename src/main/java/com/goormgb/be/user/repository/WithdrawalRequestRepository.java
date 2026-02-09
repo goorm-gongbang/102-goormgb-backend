382d28ada1641ec9cefa110871fad19783a6aad2
siyeon13 <64884565+siyeon13@users.noreply.github.com>
2026-02-09 18:14:04 +0900

    feat(Withdrawal of membership) - 회원탈퇴 API 구현 - 에러코드 Status 수정

---
+++ src/main/java/com/goormgb/be/user/repository/WithdrawalRequestRepository.java
@@ -0,0 +1,7 @@
+package com.goormgb.be.user.repository;
+
+import com.goormgb.be.user.entity.WithdrawalRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WithdrawalRequestRepository extends JpaRepository<WithdrawalRequest, Long> {
+}
