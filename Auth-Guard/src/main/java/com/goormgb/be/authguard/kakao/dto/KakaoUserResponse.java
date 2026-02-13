018a9db78ca70118e9b740f520a8c87eea450228
강슬기 <117719441+SeulGi0117@users.noreply.github.com>
2026-02-13 15:03:26 +0900

    chore: 유저 프로필 이미지 profile_image_url 필드 추가를 위한 TODO 주석 작성 (#43)

--- Auth-Guard/src/main/java/com/goormgb/be/authguard/kakao/dto/KakaoUserResponse.java
+++ Auth-Guard/src/main/java/com/goormgb/be/authguard/kakao/dto/KakaoUserResponse.java
@@ -25,29 +25,41 @@ public class KakaoUserResponse {
     private KakaoAccount kakaoAccount;
 
 
-    @Getter
-    @NoArgsConstructor
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    static class KakaoAccount {
-        private String email;
-        private Profile profile;
-    }
+        @Getter
+        @NoArgsConstructor
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        static class KakaoAccount {
+            private String email;
+            private Profile profile;
+        }
 
-    @Getter
-    @NoArgsConstructor
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    static class Profile {
-        private String nickname;
-    }
+        @Getter
+        @NoArgsConstructor
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        static class Profile {
+            private String nickname;
 
-    public String getEmail() {
-        return Optional.ofNullable(kakaoAccount).map(KakaoAccount::getEmail).orElse(null);
-    }
+            // TODO: 카카오 프로필 이미지 URL 필드 추가. 작업자: 시연
+            // @JsonProperty("profile_image_url")
+            // private String profileImageUrl;
+        }
 
-    public String getNickname() {
+        public String getEmail() {
+            return Optional.ofNullable(kakaoAccount).map(KakaoUserResponse.KakaoAccount::getEmail).orElse(null);
+        }
+
+        public String getNickname() {
         return Optional.ofNullable(kakaoAccount)         // 1단계: 계정 확인
                 .map(KakaoAccount::getProfile)           // 2단계: 프로필 확인
                 .map(Profile::getNickname)               // 3단계: 닉네임 확인
                 .orElse(null);
     }
+
+    // TODO: getProfileImageUrl() 추가 - getNickname()과 동일한 Optional 체인 패턴, 작업자: 시연
+    // public String getProfileImageUrl() {
+    //     return Optional.ofNullable(kakaoAccount)
+    //             .map(KakaoAccount::getProfile)
+    //             .map(Profile::getProfileImageUrl)
+    //             .orElse(null);
+    // }
 }
