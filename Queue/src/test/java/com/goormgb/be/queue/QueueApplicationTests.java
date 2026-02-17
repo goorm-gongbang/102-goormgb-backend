47760e499a6d65344ba5e32cec336e43c1937e81
Seulgi <jusanha17@gmail.com>
2026-02-17 15:10:32 +0900

    setting(test): 전체 모듈 테스트 환경 구성 (H2 인메모리 DB)
    - 루트 build.gradle에 H2 testRuntimeOnly 의존성 추가
    - 전체 모듈 application-test.yaml 생성 (H2, 더미 JWT/Kakao 설정)
    - 전체 모듈 ApplicationTests에 @ActiveProfiles("test") 추가

--- Queue/src/test/java/com/goormgb/be/queue/QueueApplicationTests.java
+++ Queue/src/test/java/com/goormgb/be/queue/QueueApplicationTests.java
@@ -2,8 +2,10 @@ package com.goormgb.be.queue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class QueueApplicationTests {
 	@Test
 	void contextLoads() {
