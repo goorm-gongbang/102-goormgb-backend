51927c583523b1526aabc4efda0e0c689d37023e
ejinn1 <ejinn1@naver.com>
2026-02-26 16:43:28 +0900

    feat(club): 홈 구단 전체 목록 조회 API

--- Order-Core/src/main/java/com/goormgb/be/ordercore/club/controller/ClubController.java
+++ Order-Core/src/main/java/com/goormgb/be/ordercore/club/controller/ClubController.java
@@ -2,6 +2,7 @@ package com.goormgb.be.ordercore.club.controller;
 
 import com.goormgb.be.global.response.ApiResult;
 import com.goormgb.be.ordercore.club.dto.response.ClubDetailGetResponse;
+import com.goormgb.be.ordercore.club.dto.response.ClubGetResponse;
 import com.goormgb.be.ordercore.club.service.ClubService;
 import com.goormgb.be.ordercore.match.dto.response.ClubMonthlyMatchesResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,12 +11,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Tag(name = "Club", description = "구단 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/clubs")
 public class ClubController {
     private final ClubService clubService;
+
+    @Operation(summary = "구단 전체 조회", description = "구단 전체 리스트를 조회합니다.")
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResult<ClubGetResponse> getAllClubs() {
+        return ApiResult.ok(clubService.getAllClubs());
+    }
 
     @Operation(summary = "구단 상세 조회", description = "구단 상세를 조회합니다.")
     @GetMapping("/{clubId}")
--- Order-Core/src/main/java/com/goormgb/be/ordercore/club/dto/response/ClubGetResponse.java
+++ Order-Core/src/main/java/com/goormgb/be/ordercore/club/dto/response/ClubGetResponse.java
@@ -2,20 +2,35 @@ package com.goormgb.be.ordercore.club.dto.response;
 
 import com.goormgb.be.ordercore.club.entity.Club;
 
+import java.util.List;
+
 public record ClubGetResponse(
-        Long clubId,
-        String koName,
-        String enName,
-        String logoImg,
-        String clubColor
+        List<ClubItem> clubs
 ) {
-    public static ClubGetResponse from(Club club) {
+
+    public static ClubGetResponse from(List<Club> clubs) {
         return new ClubGetResponse(
-               club.getId(),
-               club.getKoName(),
-               club.getEnName(),
-               club.getLogoImg(),
-               club.getClubColor()
+                clubs.stream()
+                        .map(ClubItem::from)
+                        .toList()
         );
+    }
+
+    public record ClubItem(
+            Long clubId,
+            String koName,
+            String enName,
+            String logoImg,
+            String clubColor
+    ) {
+        public static ClubItem from(Club club) {
+            return new ClubItem(
+                    club.getId(),
+                    club.getKoName(),
+                    club.getEnName(),
+                    club.getLogoImg(),
+                    club.getClubColor()
+            );
+        }
     }
 }
