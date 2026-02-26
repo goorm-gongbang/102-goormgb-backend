f47ce9b736d1a7fc1b7788ba4c48aefe94de1f91
ejinn1 <ejinn1@naver.com>
2026-02-26 17:18:29 +0900

    feat(match): 홈 날짜별 경기 정보 조회 API

--- Order-Core/src/main/java/com/goormgb/be/ordercore/match/controller/MatchController.java
+++ Order-Core/src/main/java/com/goormgb/be/ordercore/match/controller/MatchController.java
@@ -2,12 +2,16 @@ package com.goormgb.be.ordercore.match.controller;
 
 import com.goormgb.be.global.response.ApiResult;
 import com.goormgb.be.ordercore.match.dto.response.MatchDetailGetResponse;
+import com.goormgb.be.ordercore.match.dto.response.MatchListByDateResponse;
 import com.goormgb.be.ordercore.match.service.MatchService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @Tag(name = "Match", description = "경기 API")
 @RestController
@@ -25,4 +29,14 @@ public class MatchController {
         return ApiResult.ok(matchService.getMatchDetail(matchId));
     }
 
+    @Operation(summary = "날짜 별 경기 조회 API", description = "날짜 별 경기를 조회합니다.")
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResult<MatchListByDateResponse> getMatchList(
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate date
+    ) {
+        return ApiResult.ok(matchService.getMatchesByDate(date));
+    }
 }
