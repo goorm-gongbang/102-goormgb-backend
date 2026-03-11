f7ce6dc856dca4ee240e9245de755ddae33a8488
ejinn1 <ejinn1@naver.com>
2026-03-11 13:55:38 +0900

    refactor(common-domain): 온보딩 선호 블럭 repository 생성

---
+++ common-core/src/main/java/com/goormgb/be/domain/onboarding/repository/OnboardingPreferredBlockRepository.java
@@ -0,0 +1,9 @@
+package com.goormgb.be.domain.onboarding.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.goormgb.be.domain.onboarding.entity.OnboardingPreferredBlock;
+
+public interface OnboardingPreferredBlockRepository extends JpaRepository<OnboardingPreferredBlock, Long> {
+
+}
