d7eb82bf635876a140102ceb17f48893baa29cc3
ejinn1 <ejinn1@naver.com>
2026-03-13 17:48:22 +0900

    refactor(seat): clock 삽입을 위해서 injectMocks 어노테이션 삭제

--- Seat/src/test/java/com/goormgb/be/seat/matchSeat/service/MatchSeatPreparationServiceTest.java
+++ Seat/src/test/java/com/goormgb/be/seat/matchSeat/service/MatchSeatPreparationServiceTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -36,7 +35,6 @@ class MatchSeatPreparationServiceTest {
 
 	private Clock clock;
 
-	@InjectMocks
 	private MatchSeatPreparationService matchSeatPreparationService;
 
 	@BeforeEach
