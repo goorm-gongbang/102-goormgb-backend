f8016bc39dd721b2d4569318c4d3b4b5f838b62f
Seulgi <jusanha17@gmail.com>
2026-03-14 20:34:02 +0900

    refactor(common-core): TicketType enum을 공통 모듈로 이동
    Jira 티켓: GRGB-242

--- common-core/src/main/java/com/goormgb/be/domain/ticket/enums/TicketType.java
+++ common-core/src/main/java/com/goormgb/be/domain/ticket/enums/TicketType.java
@@ -1,4 +1,4 @@
-package com.goormgb.be.seat.pricePolicy.enums;
+package com.goormgb.be.domain.ticket.enums;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +9,7 @@ public enum TicketType {
 
 	ADULT("일반"),
 	YOUTH("청소년"),
+	MILITARY("군경"),
 	CHILD("어린이"),
 	SENIOR("경로"),
 	VETERAN("국가유공자"),
