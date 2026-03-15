2148b16be2d69a73aa2c83c2d7a21ed0fc6eca1f
Seulgi <jusanha17@gmail.com>
2026-03-15 22:04:22 +0900

    feat(seat): 좌석 배정용 에러코드 및 Repository 메서드 추가

--- Seat/src/main/java/com/goormgb/be/seat/seatHold/repository/SeatHoldRepository.java
+++ Seat/src/main/java/com/goormgb/be/seat/seatHold/repository/SeatHoldRepository.java
@@ -1,8 +1,14 @@
 package com.goormgb.be.seat.seatHold.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.goormgb.be.seat.seatHold.entity.SeatHold;
 
 public interface SeatHoldRepository extends JpaRepository<SeatHold, Long> {
+
+	List<SeatHold> findAllByUserIdAndMatchId(Long userId, Long matchId);
+
+	void deleteAllByMatchSeatIdIn(List<Long> matchSeatIds);
 }
