a0497ffa7e39fbb25940a17c94199ea74bb2a558
ejinn1 <ejinn1@naver.com>
2026-03-31 21:24:02 +0900

    chore(mypage): S3 설정 추가

---
+++ Order-Core/src/main/java/com/goormgb/be/ordercore/config/S3Config.java
@@ -0,0 +1,47 @@
+package com.goormgb.be.ordercore.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+public class S3Config {
+
+	@Bean
+	public S3Presigner s3Presigner(
+		@Value("${cloud.aws.s3.region}") String region,
+		@Value("${cloud.aws.credentials.access-key}") String accessKey,
+		@Value("${cloud.aws.credentials.secret-key}") String secretKey
+	) {
+		StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+			AwsBasicCredentials.create(accessKey, secretKey)
+		);
+
+		return S3Presigner.builder()
+			.region(Region.of(region))
+			.credentialsProvider(credentialsProvider)
+			.build();
+	}
+
+	@Bean
+	public S3Client s3Client(
+		@Value("${cloud.aws.s3.region}") String region,
+		@Value("${cloud.aws.credentials.access-key}") String accessKey,
+		@Value("${cloud.aws.credentials.secret-key}") String secretKey
+	) {
+		StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+			AwsBasicCredentials.create(accessKey, secretKey)
+		);
+
+		return S3Client.builder()
+			.region(Region.of(region))
+			.credentialsProvider(credentialsProvider)
+			.build();
+	}
+}
