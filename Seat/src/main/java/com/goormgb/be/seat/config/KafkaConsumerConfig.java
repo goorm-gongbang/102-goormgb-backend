b1d7be4956badf289302059d4af71430aa34e42c
유의진 <101779861+ejinn1@users.noreply.github.com>
2026-04-09 11:57:08 +0900

    feat(kafka): Consumer DLT 에러 처리 및 안정화 (Seat + Order-Core) [GRGB-348] (#262)
    * feat(common-core): DLT 토픽 생성
    
    * feat(seat): DLT producer 추가, common error handling 추가
    
    * feat(order-core): common error handling 추가
    
    * feat(seat): seat consumer 로그 표준화
    
    * feat(order-core): email consumer 로그 표준화
    
    * refactor(seat): 들여쓰기 수정

---
+++ Seat/src/main/java/com/goormgb/be/seat/config/KafkaConsumerConfig.java
@@ -0,0 +1,25 @@
+package com.goormgb.be.seat.config;
+
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+	@Bean
+	public CommonErrorHandler commonErrorHandler(KafkaTemplate<String, Object> kafkaTemplate) {
+		DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(
+			kafkaTemplate,
+			(record, ex) -> new TopicPartition(record.topic() + ".DLT", record.partition())
+		);
+
+		// 1초 간격 3회 재시도 후 DLT 전송
+		return new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 3L));
+	}
+}
